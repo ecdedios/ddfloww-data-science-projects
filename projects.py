bc171a78805b8524b04ce5c31887627ae0cc2b4b
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2020-03-22 09:57:56 -0500

    add dv and sso project indexes

--- projects.py
+++ projects.py
@@ -1,9 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data-science-projects/domestic-violence-1')
+def show_domestic_violence_1():
+    return render_template('/domestic-violence-1/index.html')
+
+@app.route('/data-science-projects/datathon-2019')
+def show_datathon_2019():
+    return render_template('/datathon-2019/index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
