3f25246d180a485e1276d9563e048567fb0b1b86
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2020-03-22 22:26:08 -0500

    added prediction functionality

--- projects.py
+++ projects.py
@@ -11,12 +11,8 @@ def index():
 def show_domestic_violence_2019():
     return render_template('/domestic-violence-2019/index.html')
 
-@app.route('/data-science-projects/datathon-2019')
-def show_datathon_2019():
-    return render_template('/datathon-2019/index.html')
-
-@app.route('/data-science-projects/domestic-violence-2019/submit', methods=['GET', 'POST'])
-def result_domestic_violence_2019():
+@app.route('/data-science-projects/domestic-violence-2019/submit_dv', methods=['GET', 'POST'])
+def submit_dv():
     error = None
     if request.method == 'POST':
         if request.form['q1'] == 'x':
@@ -32,14 +28,14 @@ def result_domestic_violence_2019():
             feature8 = int(float(request.form['q8']))
             feature9 = int(float(request.form['q9']))
             prediction = domestic_violence_2019.predictorizer(feature1,
-                                                              feature2,
-                                                              feature3,
-                                                              feature4,
-                                                              feature5,
-                                                              feature6,
-                                                              feature7,
-                                                              feature8,
-                                                              feature9)
+                                                feature2,
+                                                feature3,
+                                                feature4,
+                                                feature5,
+                                                feature6,
+                                                feature7,
+                                                feature8,
+                                                feature9)
             return render_template('/domestic-violence-2019/submit.html',
                                     prediction=prediction,
                                     feature1=feature1,
@@ -52,6 +48,8 @@ def result_domestic_violence_2019():
                                     feature8=feature8,
                                     feature9=feature9,)        
         return render_template('/domestic-violence-2019/index.html', error=error)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
